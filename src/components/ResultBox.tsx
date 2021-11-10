8ad65c8af4a7b9f26bc233cee1bdcf6ad725abd3
waniroukun <wanirou14titech@gmail.com>
2021-11-10 17:50:46 +0900

    ノーツを2列にしてみる

--- src/components/ResultBox.tsx
+++ src/components/ResultBox.tsx
@@ -4,9 +4,9 @@ import {
   Box,
 } from "@material-ui/core";
 
-const FIGURE_CONTAINTER_WIDTH = 200;
 const FIGURE_CONTAINTER_HEIGHT = 360;
 const FIGURE_HEIGHT = 320;
+const NOTE_WIDTH = 200;
 
 const useStyles = makeStyles(() => ({
   containerBox: {
@@ -15,29 +15,33 @@ const useStyles = makeStyles(() => ({
     justifyContent: 'space-evenly',
   },
   figureContainer: {
-    width: FIGURE_CONTAINTER_WIDTH,
     height: FIGURE_CONTAINTER_HEIGHT,
     position: 'relative',
   },
-  noteFigure: {
-    top: 0,
-    right: 0,
-    bottom: 0,
-    left: 0,
+  noteContainer: {
+    height: FIGURE_CONTAINTER_HEIGHT,
+    display: 'flex',
+  },
+  realNoteFigure1: {
     margin: 'auto',
-    width: '80%',
-    borderTop: '1rem solid #696969',
+    marginRight: 20,
+    width: NOTE_WIDTH,
     borderBottom: '1rem solid #696969',
-    position: 'absolute',
+  },
+  realNoteFigure2: {
+    margin: 'auto',
+    marginLeft: 20,
+    width: NOTE_WIDTH,
+    borderTop: '1rem solid #696969',
   },
   criticalFigure: {
-    top: 0,
-    right: 0,
-    bottom: 0,
-    left: 0,
-    margin: 'auto',
-    width: '80%',
-    background: '#ffd800',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    WebkitTransform: 'translate(-50%, -50%)',
+    MsTransform: 'translate(-50%, -50%)',
+    width: '100%',
+    background: '#f90',
     opacity: 0.7,
     position: 'absolute',
     textAlign: 'center',
@@ -85,18 +89,26 @@ const ResultBox: React.FC<ResultBoxProps> = (props: ResultBoxProps) => {
   }
 
   return (
-    <Box className={classes.containerBox}>
-      <Box className={classes.figureContainer}>
-        {criticalSec > 0 && (
-          <Box
-            className={classes.criticalFigure}
-            sx={{ height: criticalFigureHeight }}
-          />
-        )}
-        <Box
-          className={classes.noteFigure}
-          sx={{ height: noteFigureHeight }}
-        />
+    <>
+      <Box className={classes.containerBox}>
+        <Box className={classes.figureContainer}>
+          {criticalSec > 0 && (
+            <Box
+              className={classes.criticalFigure}
+              sx={{ height: criticalFigureHeight }}
+            />
+          )}
+          <Box className={classes.noteContainer}>
+            <Box
+              className={classes.realNoteFigure1}
+              sx={{ height: noteFigureHeight }}
+            />
+            <Box
+              className={classes.realNoteFigure2}
+              sx={{ height: noteFigureHeight }}
+            />
+          </Box>
+        </Box>
       </Box>
 
       <Box>
@@ -116,7 +128,7 @@ const ResultBox: React.FC<ResultBoxProps> = (props: ResultBoxProps) => {
           </div>
         </Box>
       </Box>
-    </Box>
+    </>
   );
 };
 
