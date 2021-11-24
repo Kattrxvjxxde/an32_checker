5a7abc99f7d974291b6d4a75bb878c438e8208f6
waniroukun <wanirou14titech@gmail.com>
2021-11-24 13:09:25 +0900

    イメージ図改良

--- src/components/ResultBox.tsx
+++ src/components/ResultBox.tsx
@@ -5,10 +5,7 @@ import {
 } from "@material-ui/core";
 import SupplementButton from './SupplementButton';
 import TweetButton from './TweetButton';
-
-const FIGURE_CONTAINTER_HEIGHT = 320;
-const FIGURE_HEIGHT = 240;
-const NOTE_WIDTH = 120;
+import ImageFigure from './ImageFigure';
 
 const CRITICAL_TH0 = 1.0 * 1000 / 120;
 const CRITICAL_TH1 = 3.0 * 1000 / 120;
@@ -23,63 +20,13 @@ const useStyles = makeStyles(() => ({
     justifyContent: 'space-evenly',
     border: '0.2rem solid #dcdcdc',
   },
-  figureContainer: {
-    height: FIGURE_CONTAINTER_HEIGHT,
-    position: 'relative',
-  },
-  noteContainer: {
-    height: FIGURE_CONTAINTER_HEIGHT,
+  imageFigureBox: {
+    marginBottom: '0.9rem',
+    padding: '0.7rem',
     display: 'flex',
-  },
-  realNoteFigure1: {
-    margin: 'auto',
-    marginRight: 16,
-    width: NOTE_WIDTH,
-    borderBottom: '0.8rem solid #696969',
-    zIndex: 100,
-  },
-  realNoteFigure2: {
-    margin: 'auto',
-    marginLeft: 16,
-    width: NOTE_WIDTH,
-    borderTop: '0.8rem solid #696969',
-    zIndex: 100,
-  },
-  criticalFigure: {
-    top: '50%',
-    left: '50%',
-    transform: 'translate(-50%, -50%)',
-    WebkitTransform: 'translate(-50%, -50%)',
-    MsTransform: 'translate(-50%, -50%)',
-    width: '100%',
-    position: 'absolute',
-    textAlign: 'center',
-    animation: '$flashFigure linear infinite 1s',
-  },
-  '@keyframes flashFigure': {
-    '0%, 100%': { opacity: 0.5 },
-    '50%': { opacity: 0.3 },
-  },
-  impossibleText: {
-    top: '38%',
-    transform: 'rotate(12deg)',
-    WebkitTransform: 'rotate(12deg)',
-    MsTransform: 'rotate(12deg)',
-    width: '100%',
-    position: 'absolute',
-    textAlign: 'center',
-    fontSize: '3.1rem',
-    fontWeight: 'bold',
-    color: '#999',
-  },
-  virtualNote: {
-    top: '50%',
-    left: '50%',
-    transform: 'translate(-50%, -50%)',
-    WebkitTransform: 'translate(-50%, -50%)',
-    MsTransform: 'translate(-50%, -50%)',
-    width: '100%',
-
+    justifyContent: 'space-evenly',
+    border: '0.2rem solid #dcdcdc',
+    backgroundColor: 'rgba(0, 0, 10, 0.8)',
   },
   resultContainer: {
     marginBottom: '3.2rem',
@@ -105,7 +52,7 @@ const useStyles = makeStyles(() => ({
     },
   },
   description: {
-    fontSize: '0.8rem',
+    fontSize: '0.6rem',
     textAlign: 'center',
   },
   resultText: {
@@ -133,16 +80,6 @@ const ResultBox: React.FC<ResultBoxProps> = (props: ResultBoxProps) => {
   const classes = useStyles();
   const { bpm, noteType, interval, criticalSec } = props;
 
-  const noteFigureHeight =
-    interval > criticalSec
-      ? FIGURE_HEIGHT
-      : FIGURE_HEIGHT * interval / criticalSec;
-
-  const criticalFigureHeight =
-   criticalSec > interval
-      ? FIGURE_HEIGHT
-      : FIGURE_HEIGHT * criticalSec / interval;
-
   const intervalStr = interval === 0 ? 'ERROR' : `${interval} ms`
 
   let criticalSecStr = 'ERROR';
@@ -151,7 +88,6 @@ const ResultBox: React.FC<ResultBoxProps> = (props: ResultBoxProps) => {
   }
 
   let criticalTextColor = '#666';
-  let criticalFigureColor = 'repeating-linear-gradient(-45deg, #666, #666 5px, #ccc 5px, #ccc 10px)';
   let resultText = '正常となるような値を指定してください。';
 
   if (interval !== 0) {
@@ -159,29 +95,24 @@ const ResultBox: React.FC<ResultBoxProps> = (props: ResultBoxProps) => {
       resultText = '餡蜜は不可能です。';
     } else if (0 <= criticalSec && criticalSec < CRITICAL_TH0) {
       criticalTextColor = '#666';
-      criticalFigureColor = 'repeating-linear-gradient(-45deg, #666, #666 5px, #bbb 5px, #bbb 10px)';
       resultText = '餡蜜の有用性はほぼありません。\n頑張って譜面通りに叩きましょう。';
     } else if (CRITICAL_TH0 <= criticalSec && criticalSec < CRITICAL_TH1) {
       criticalTextColor = '#05f';
-      criticalFigureColor = 'repeating-linear-gradient(-45deg, #05f, #05f 5px, #7bf 5px, #7bf 10px)';
       resultText = noteType === 24
         ? '餡蜜の有用性は低いです。\n24分を16分に変換できる譜面（※）であっても、\n相当精度に自信がない限りは、頑張って譜面通りに叩きましょう。'
         : '餡蜜の有用性は低いです。\n頑張って譜面通りに叩きましょう。';
     } else if (CRITICAL_TH1 <= criticalSec && criticalSec < CRITICAL_TH2) {
       criticalTextColor = '#0c0';
-      criticalFigureColor = 'repeating-linear-gradient(-45deg, #0c0, #0c0 5px, #8e8 5px, #8e8 10px)';
       resultText = noteType === 24
         ? '餡蜜の有用性はそこそこあります。\n24分を16分に変換できる譜面(※)であれば、\n餡蜜してみるのもよいでしょう。'
         : '餡蜜の有用性はそこそこあります。\nしかしながら、できる限りは頑張って譜面通りに叩くのが良さそうです。';
     } else if (CRITICAL_TH2 <= criticalSec && criticalSec < CRITICAL_TH3) {
       criticalTextColor = '#f70';
-      criticalFigureColor = 'repeating-linear-gradient(-45deg, #f70, #f70 5px, #fc5 5px, #fc5 10px)';
       resultText = noteType === 24
         ? '餡蜜の有用性は高いです！\n24分を16分に変換できる譜面(※)であれば、\n安定してCRITICAL判定が出るでしょう。'
         : '餡蜜の有用性は高いです！\n1個目のノートからほんの少し遅入りして餡蜜する感覚に慣れたら、\nCRITICAL判定を安定させることができるでしょう。';
     } else {
       criticalTextColor = '#f22';
-      criticalFigureColor = 'repeating-linear-gradient(-45deg, #f22, #f22 5px, #f99 5px, #f99 10px)';
       resultText = '餡蜜の有用性は非常に高いです!!\n特に意識せずとも餡蜜で安定してCRITICAL判定が出るでしょう。\nノーツの中央でなく、どちらかのノーツに合わせた餡蜜でもおおよそ大丈夫です。';
     }
   }
@@ -234,47 +165,27 @@ const ResultBox: React.FC<ResultBoxProps> = (props: ResultBoxProps) => {
             イメージ図
           </Box>
 
-          <Box className={classes.flexBox}>
-            <Box className={classes.figureContainer}>
-              <Box className={classes.noteContainer}>
-                <Box
-                  className={classes.realNoteFigure1}
-                  sx={{ height: noteFigureHeight }}
-                />
-                <Box
-                  className={classes.realNoteFigure2}
-                  sx={{ height: noteFigureHeight }}
-                />
-              </Box>
-              {criticalSec > 0 && (
-                <Box
-                  className={classes.criticalFigure}
-                  sx={{ height: criticalFigureHeight }}
-                  style={{ background: criticalFigureColor }}
-                />
-              )}
-              {criticalSec <= 0 && (
-                <Box className={classes.impossibleText}>
-                  餡蜜不可能
-                </Box>
-              )}
-            </Box>
+          <Box className={classes.imageFigureBox}>
+            <ImageFigure
+              interval={interval}
+              criticalSec={criticalSec}
+            />
           </Box>
 
           <Box className={classes.description}>
             <p>
-              2本ある黒い棒がノーツを表しています。<br />
-              左側が餡蜜したいノーツの1個目、右側が2個目です。<br />
-              この2本の黒い棒の間隔が<b>ノーツ間隔</b>に相当します。
+              左下と右上にある白い棒は、ノーツを表しています。<br />
+              この2本の白い棒の間隔が<b>ノーツ間隔</b>に相当します。
             </p>
             <p>
-              斜線で塗り潰された長方形が、<br />
-              餡蜜した場合にCRITICAL判定で光る範囲を表しています。<br />
+              ノーツの周囲にある黄色く塗られた部分が、それぞれのノートのCRITICAL判定の範囲（前後それぞれ41.667 [ms]）です。<br />
+              2つのノーツのCRITICAL判定の重複部分が、赤い斜線で塗られています。<br />
+              この赤い斜線で塗られた長方形が、餡蜜した場合にCRITICAL判定で光る範囲であり、<br />
               この長方形の高さが<b>CRITICAL範囲</b>に相当します。
               {criticalSec <= 0 && (
                 <>
                   <br />
-                  （長方形がない場合、餡蜜した場合にCRITICAL判定で光る範囲はありません。）
+                  （赤い斜線で塗られた長方形がない場合、餡蜜した場合にCRITICAL判定で光る範囲はありません。）
                 </>
               )}
             </p>
